b329f50e62282e5a3698b7f5695e0f0a512761c6
chenzhuoji <chenzhuo-ji@qq.com>
2026-05-15 11:27:54 +0800

    style: swap sidebar order - TOC above column list

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -126,12 +126,12 @@ export default function Post() {
           <RelatedArticles articles={related} />
         </div>
         <aside className="hidden lg:flex flex-col gap-6 sticky top-24 w-56 shrink-0 h-[calc(100vh-6rem)]">
+          <TOC post={post} />
           <div className="flex-1 min-h-0 overflow-y-auto scrollbar-thin">
             {post.meta.column && (
               <ColumnSidebar column={post.meta.column} currentSlug={post.meta.slug} />
             )}
           </div>
-          <TOC post={post} />
         </aside>
       </div>
     </article>
